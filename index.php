1ffb53e2695d2c616a88b08749348b1646796ae7
John Griffin <jgriffin407@gmail.com>
2020-05-14 20:47:53 -0400

    edited index file

--- index.php
+++ index.php
@@ -17,29 +17,29 @@
             <h1>Available Trivia Games</h1>
     <ul>
         <h3>General Knowledge</h3>
-        <li><a class="gamelink" href="game/general/easy/gen_easy.html">Easy</a></li><br/><br/>
-        <li><a class="gamelink" href="game/general/medium/gen_med.html">Medium</a></li><br/><br/>
-        <li><a class="gamelink" href="game/general/hard/gen_hard.html">Hard</a></li><br/><br/>
+        <li><a class="gamelink" href="game/general/easy/gen_easy.php">Easy</a></li><br/><br/>
+        <li><a class="gamelink" href="game/general/medium/gen_med.php">Medium</a></li><br/><br/>
+        <li><a class="gamelink" href="game/general/hard/gen_hard.php">Hard</a></li><br/><br/>
         <h3>Movies</h3>
-        <li><a class="gamelink" href="game/movies/easy/mov_easy.html">Easy</a></li><br/><br/>
-        <li><a class="gamelink" href="game/movies/medium/mov_med.html">Medium</a></li><br/><br/>
-        <li><a class="gamelink" href="game/movies/hard/mov_hard.html">Hard</a></li><br/><br/>
+        <li><a class="gamelink" href="game/movies/easy/mov_easy.php">Easy</a></li><br/><br/>
+        <li><a class="gamelink" href="game/movies/medium/mov_med.php">Medium</a></li><br/><br/>
+        <li><a class="gamelink" href="game/movies/hard/mov_hard.php">Hard</a></li><br/><br/>
         <h3>Music</h3>
-        <li><a class="gamelink" href="game/music/easy/mus_easy.html">Easy</a></li><br/><br/>
-        <li><a class="gamelink" href="game/music/medium/mus_med.html">Medium</a></li><br/><br/>
-        <li><a class="gamelink" href="game/music/hard/mus_hard.html">Hard</a></li><br/><br/>
+        <li><a class="gamelink" href="game/music/easy/mus_easy.php">Easy</a></li><br/><br/>
+        <li><a class="gamelink" href="game/music/medium/mus_med.php">Medium</a></li><br/><br/>
+        <li><a class="gamelink" href="game/music/hard/mus_hard.php">Hard</a></li><br/><br/>
         <h3>Politics</h3>
-        <li><a class="gamelink" href="game/politics/easy/pol_easy.html">Easy</a></li><br/><br/>
-        <li><a class="gamelink" href="game/politics/medium/pol_med.html">Medium</a></li><br/><br/>
-        <li><a class="gamelink" href="game/politics/hard/pol_hard.html">Hard</a></li><br/><br/>
+        <li><a class="gamelink" href="game/politics/easy/pol_easy.php">Easy</a></li><br/><br/>
+        <li><a class="gamelink" href="game/politics/medium/pol_med.php">Medium</a></li><br/><br/>
+        <li><a class="gamelink" href="game/politics/hard/pol_hard.php">Hard</a></li><br/><br/>
         <h3>Sports</h3>
-        <li><a class="gamelink" href="game/sports/easy/spo_easy.html">Easy</a></li><br/><br/>
-        <li><a class="gamelink" href="game/sports/medium/spo_med.html">Medium</a></li><br/><br/>
-        <li><a class="gamelink" href="game/sports/hard/spo_hard.html">Hard</a></li><br/><br/>
+        <li><a class="gamelink" href="game/sports/easy/spo_easy.php">Easy</a></li><br/><br/>
+        <li><a class="gamelink" href="game/sports/medium/spo_med.php">Medium</a></li><br/><br/>
+        <li><a class="gamelink" href="game/sports/hard/spo_hard.php">Hard</a></li><br/><br/>
         <h3>Television</h3>
-        <li><a class="gamelink" href="game/television/easy/tel_easy.html">Easy</a></li><br/><br/>
-        <li><a class="gamelink" href="game/television/medium/tel_med.html">Medium</a></li><br/><br/>
-        <li><a class="gamelink" href="game/television/hard/tel_hard.html">Hard</a></li><br/><br/>
+        <li><a class="gamelink" href="game/television/easy/tel_easy.php">Easy</a></li><br/><br/>
+        <li><a class="gamelink" href="game/television/medium/tel_med.php">Medium</a></li><br/><br/>
+        <li><a class="gamelink" href="game/television/hard/tel_hard.php">Hard</a></li><br/><br/>
     </ul>
         </div>
     <div class="footer">
